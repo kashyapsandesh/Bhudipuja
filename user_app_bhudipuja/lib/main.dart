384eee15431351dd32d504677472e6de8d354003
Sandesh Ghimire <sandeshghimire202@gmail.com>
2024-03-12 13:31:40 +0545

    user_app_bhudipuja_completed

---
+++ user_app_bhudipuja/lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:user_app_bhudipuja/assistant_methods/address_changer.dart';
+import 'package:user_app_bhudipuja/assistant_methods/cart_item_counter.dart';
+import 'package:user_app_bhudipuja/assistant_methods/total_ammount.dart';
+import 'package:user_app_bhudipuja/firebase_options.dart';
+import 'package:user_app_bhudipuja/splashScreen/splash_screen.dart';
+
+import 'global/global.dart';
+
+Future<void> main()async  {
+  WidgetsFlutterBinding.ensureInitialized();
+  sharedPreferences = await SharedPreferences.getInstance();
+await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => CartItemCounter()),
+        ChangeNotifierProvider(create: (context) => TotalAmmount()),
+        ChangeNotifierProvider(create: (context) => AddressChanger()),
+      ],
+      child: MaterialApp(
+        title: 'Users App',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const MySplashScreen(),
+      ),
+    );
+  }
+}
